a843237258cdbc1019dbf38458ac0f89b169c080
Rens Saladaga <staleexam19@gmail.com>
2025-07-31 07:02:59 +0800

    solution

--- index.js
+++ index.js
@@ -20,9 +20,38 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// app.get("/api/hello", function (req, res) {
+//   res.json({greeting: 'hello API'});
+// });
+
+app.get("/api/:date?", (req, res) => {
+  let dateInput = req.params.date;
+
+  let date;
+
+  if (!dateInput) {
+    date = new Date();
+  } else {
+
+
+    if (!isNaN(dateInput)) {
+      date = new Date(parseInt(dateInput));
+    } else {
+      date = new Date(dateInput)
+    }
+  }
+
+  if (date.toString() === "Invalid Date") {
+    return res.json({
+      error: "Invalid Date"
+    });
+  }
+
+  return res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
+})
 
 
 
